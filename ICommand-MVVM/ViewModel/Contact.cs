71f45e56efcd344e876b6b553b89166715884dbd
Vadim <chorrny228@gmail.com>
2021-06-01 17:33:36 +0300

    Add project files.

---
+++ ICommand-MVVM/ViewModel/Contact.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICommand_MVVM
+{
+    public class Contact : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Name
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged(); // call event for update name property
+                OnPropertyChanged(nameof(Fullname));
+            }
+
+        }
+
+        // Surname
+        private string surname;
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                if(surname != value)
+                {
+                    surname = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(Fullname));
+                }
+            }
+        }
+
+        // Age
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+               if(age < 100 && age != value)
+                {
+                    age = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        // Phone
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                if(phone != value)
+                {
+                    phone = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        // City
+        
+
+        // isMale
+        private bool isMale;
+        public bool IsMale
+        {
+            get { return isMale; }
+            set
+            {
+                if (isMale != value)
+                {
+                    isMale = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(GenderName));
+                }
+            }
+        }
+
+        public string GenderName => IsMale ? "Male" : "Female";
+
+        public string Fullname => $"{Name} {Surname}";
+
+        public object Clone()
+        {
+            // shallow copy (поверхневе копіювання) - копіюються лише 
+            // значення value типів та посилання reference типів
+            Contact clone = (Contact)this.MemberwiseClone();
+
+            // deep copy (глибоке копіювання) - кожний reference тип
+            // копіюється власним методом клонування
+            clone.Name = (string)this.Name.Clone();
+            clone.Surname = (string)this.Surname.Clone();
+            clone.Phone = (string)this.Phone.Clone();
+
+            return clone;
+        }
+
+        // func for change properties callermemebers
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
---
+++ ICommand-MVVM/ViewModel/ViewModel.cs
@@ -0,0 +1,95 @@
+﻿using ICommand_MVVM.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ICommand_MVVM
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Contact selectedContact;
+        // collections with contacts
+        private readonly ICollection<Contact> contacts = new ObservableCollection<Contact>();
+       
+        private Command copyContactCommand;
+        private Command removeContactCommand;
+
+        public ViewModel()
+        {
+            contacts.Add(new Contact() { Name = "Вова", Age = 30, Surname = "Зеленський", Phone = "+3809934532123", IsMale = true });
+            contacts.Add(new Contact() { Name = "Рінат", Age = 25, Surname = "Ахметов", Phone = "+38037124224", IsMale = false });
+            contacts.Add(new Contact() { Name = "Беня(Ігор)", Age = 33, Surname = "Коломойській", Phone = "+38063285792", IsMale = false });
+            copyContactCommand = new DelegateCommand(DublicateSelectedContact, IsCanSetCreate);
+            removeContactCommand = new DelegateCommand(RemoveSelectedContact, IsCanSetRemove);
+
+            // встановлення обробника на подію зміни властивості 
+            PropertyChanged += (s, a) =>
+            {
+                if (a.PropertyName == nameof(SelectedContact))
+                {
+                    copyContactCommand.RaiseCanExecuteChanged();
+                    removeContactCommand.RaiseCanExecuteChanged();
+                }
+            };
+        }
+
+        // for binding elements on windows
+        public IEnumerable<Contact> Contacts => contacts;
+
+        // change property
+        public Contact SelectedContact
+        {
+            get => selectedContact;
+            set
+            {
+                selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CopyContactCommand => copyContactCommand;
+        public ICommand RemoveContactCommand => removeContactCommand;
+
+        bool IsCanSetCreate()
+        {
+            return SelectedContact != null;
+        }
+        // Метод дублювання вибраного контакта
+        public void DublicateSelectedContact()
+        {
+            if (SelectedContact != null)
+            {
+                contacts.Add((Contact)SelectedContact.Clone()); // on contacts func
+            }
+        }
+
+        bool IsCanSetRemove()
+        {
+            return SelectedContact != null;
+        }
+        public void RemoveSelectedContact()
+        {
+            if (SelectedContact != null)
+                contacts.Remove(SelectedContact);
+        }
+
+        bool IsCanOnlyCreate()
+        {
+            return true;
+        }
+
+
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
